9f1be0923c3ed8b582b1287064d29759b972aa01
Ismael Victor <ivictormc29@gmail.com>
2025-09-09 13:23:00 +0000

    09/09

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 0;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+    printf("numero digitado: %d\n", numero);
+
+    return 0;
+}
