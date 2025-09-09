f94026855d81f5f842a156975077811b6a445b47
Ismael Victor <ivictormc29@gmail.com>
2025-09-09 13:30:31 +0000

    09/09 Tabuada em C

--- hello.c
+++ hello.c
@@ -8,5 +8,10 @@ int main() {
 
     printf("numero digitado: %d\n", numero);
 
+    for(int mult = 1; mult <= 10; mult++) {
+        printf("%d * %d = %d\n", mult, numero, (mult * numero));
+
+    }
+
     return 0;
 }
